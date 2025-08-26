b51a8da17f326e4f76c7bd91205e1ce3435e6462
AIMILION <info@aimilion.com>
2025-08-26 15:47:18 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 // app/layout.tsx
-import "./globals.css"; // jei turi globalius css
+import "./globals.css";
+import type { Metadata } from "next";
+
+// Čia gali aprašyti SEO metadata
+export const metadata: Metadata = {
+  title: "AIMILION",
+  description: "GOING WITH LIONS",
+};
 
 export default function RootLayout({
   children,
